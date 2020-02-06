7c841348fccc4f7f658ef26d5737fd80dcb260b9
Kishor Datta Gupta <kishordgupta@users.noreply.github.com>
2020-02-06 03:25:59 -0600

    Create test.py
    test

---
+++ test.py
@@ -0,0 +1,162 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import logging
+
+from keras.models import Sequential
+from keras.layers import Dense, Flatten, Conv2D, MaxPooling2D, Activation, Dropout
+import numpy as np
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Conv2D, MaxPooling2D, BatchNormalization
+from keras import optimizers
+import numpy as np
+from keras.layers.core import Lambda
+from keras import backend as K
+from keras import regularizers
+from art.attacks import DeepFool
+from art.classifiers import KerasClassifier
+from art.utils import load_dataset
+
+	
+num_classes = 10
+weight_decay = 0.0005
+x_shape = [32,32,3]
+model = Sequential()
+weight_decay = weight_decay
+
+model.add(Conv2D(64, (3, 3), padding='same',
+				 input_shape=x_shape,kernel_regularizer=regularizers.l2(weight_decay)))
+model.add(Activation('relu'))
+model.add(BatchNormalization())
+model.add(Dropout(0.3))
+
+model.add(Conv2D(64, (3, 3), padding='same',kernel_regularizer=regularizers.l2(weight_decay)))
+model.add(Activation('relu'))
+model.add(BatchNormalization())
+
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(128, (3, 3), padding='same',kernel_regularizer=regularizers.l2(weight_decay)))
+model.add(Activation('relu'))
+model.add(BatchNormalization())
+model.add(Dropout(0.4))
+
+model.add(Conv2D(128, (3, 3), padding='same',kernel_regularizer=regularizers.l2(weight_decay)))
+model.add(Activation('relu'))
+model.add(BatchNormalization())
+
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(256, (3, 3), padding='same',kernel_regularizer=regularizers.l2(weight_decay)))
+model.add(Activation('relu'))
+model.add(BatchNormalization())
+model.add(Dropout(0.4))
+
+model.add(Conv2D(256, (3, 3), padding='same',kernel_regularizer=regularizers.l2(weight_decay)))
+model.add(Activation('relu'))
+model.add(BatchNormalization())
+model.add(Dropout(0.4))
+
+model.add(Conv2D(256, (3, 3), padding='same',kernel_regularizer=regularizers.l2(weight_decay)))
+model.add(Activation('relu'))
+model.add(BatchNormalization())
+
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+
+model.add(Conv2D(512, (3, 3), padding='same',kernel_regularizer=regularizers.l2(weight_decay)))
+model.add(Activation('relu'))
+model.add(BatchNormalization())
+model.add(Dropout(0.4))
+
+model.add(Conv2D(512, (3, 3), padding='same',kernel_regularizer=regularizers.l2(weight_decay)))
+model.add(Activation('relu'))
+model.add(BatchNormalization())
+model.add(Dropout(0.4))
+
+model.add(Conv2D(512, (3, 3), padding='same',kernel_regularizer=regularizers.l2(weight_decay)))
+model.add(Activation('relu'))
+model.add(BatchNormalization())
+
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+
+model.add(Conv2D(512, (3, 3), padding='same',kernel_regularizer=regularizers.l2(weight_decay)))
+model.add(Activation('relu'))
+model.add(BatchNormalization())
+model.add(Dropout(0.4))
+
+model.add(Conv2D(512, (3, 3), padding='same',kernel_regularizer=regularizers.l2(weight_decay)))
+model.add(Activation('relu'))
+model.add(BatchNormalization())
+model.add(Dropout(0.4))
+
+model.add(Conv2D(512, (3, 3), padding='same',kernel_regularizer=regularizers.l2(weight_decay)))
+model.add(Activation('relu'))
+model.add(BatchNormalization())
+
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.5))
+
+model.add(Flatten())
+model.add(Dense(512,kernel_regularizer=regularizers.l2(weight_decay)))
+model.add(Activation('relu'))
+model.add(BatchNormalization())
+
+model.add(Dropout(0.5))
+model.add(Dense(num_classes))
+model.add(Activation('softmax'))
+
+# Configure a logger to capture ART outputs; these are printed in console and the level of detail is set to INFO
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+handler = logging.StreamHandler()
+formatter = logging.Formatter('[%(levelname)s] %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+# Read CIFAR10 dataset
+(x_train, y_train), (x_test, y_test), min_, max_ = load_dataset(str('cifar10'))
+x_train, y_train = x_train[:5000], y_train[:5000]
+x_test, y_test = x_test[:500], y_test[:500]
+im_shape = x_train[0].shape
+
+
+# Create Keras convolutional neural network - basic architecture from Keras examples
+# Source here: https://github.com/keras-team/keras/blob/master/examples/cifar10_cnn.py
+
+
+
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+# Create classifier wrapper
+classifier = KerasClassifier(model=model, clip_values=(min_, max_))
+classifier.fit(x_train, y_train, nb_epochs=10, batch_size=128)
+
+# Craft adversarial samples with DeepFool
+logger.info('Create DeepFool attack')
+adv_crafter = DeepFool(classifier)
+logger.info('Craft attack on training examples')
+x_train_adv = adv_crafter.generate(x_train)
+logger.info('Craft attack test examples')
+x_test_adv = adv_crafter.generate(x_test)
+
+# Evaluate the classifier on the adversarial samples
+preds = np.argmax(classifier.predict(x_test_adv), axis=1)
+acc = np.sum(preds == np.argmax(y_test, axis=1)) / y_test.shape[0]
+logger.info('Classifier before adversarial training')
+logger.info('Accuracy on adversarial samples: %.2f%%', (acc * 100))
+
+# Data augmentation: expand the training set with the adversarial samples
+x_train = np.append(x_train, x_train_adv, axis=0)
+y_train = np.append(y_train, y_train, axis=0)
+
+# Retrain the CNN on the extended dataset
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+classifier.fit(x_train, y_train, nb_epochs=10, batch_size=128)
+
+# Evaluate the adversarially trained classifier on the test set
+preds = np.argmax(classifier.predict(x_test_adv), axis=1)
+acc = np.sum(preds == np.argmax(y_test, axis=1)) / y_test.shape[0]
+logger.info('Classifier with adversarial training')
+logger.info('Accuracy on adversarial samples: %.2f%%', (acc * 100))
